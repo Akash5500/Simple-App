b05e64579f5bbc2b7601aa41e13b9504427468ad
Akash Hudedh <70318094+Akash5500@users.noreply.github.com>
2024-08-05 16:51:37 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -2,4 +2,11 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+CONTAINER_ID=$(docker ps -q --filter "ancestor=akash5500/simple-python-flask-app")
+
+if [ -n "$CONTAINER_ID" ]; then
+  echo "Stopping container $CONTAINER_ID"
+  docker stop $CONTAINER_ID
+else
+  echo "No running container found for image akash5500/simple-python-flask-app"
+fi
